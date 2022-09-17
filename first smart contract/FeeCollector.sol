eea2ac6e44eb180da595b427274af7f9ef248d50
Ahmet ESMER <92627988+AHMET-ESMER14@users.noreply.github.com>
2022-09-18 01:01:12 +0300

    Add files via upload

---
+++ first smart contract/FeeCollector.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
+
+contract FeeCollector {
+    address public owner;
+    uint256 public balance;
+
+    constructor(){
+        owner = msg.sender;
+    }
+    
+    receive() payable external {
+        balance += msg.value;
+    }
+
+    function withdraw(uint amount,address payable destAddr)public{
+
+        require(msg.sender == owner,"Only owner can withdraw please");
+        require(amount <= balance, "Insufficient funds");
+        destAddr.transfer(amount);
+        balance -= amount;
+    }
+}
+
+
+
